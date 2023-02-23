1a6b65ac0abfa14ecbb6285ef86ec0e33cf18f2d
darksip <pierre.siccardi@gmail.com>
2023-02-23 17:06:25 +0100

    menu refactoring with abstraction

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"strings"
 	"sync"
 	"time"
 
@@ -36,26 +35,15 @@ var (
 // il faudrait faire une struct pour refleter l etat de la struct dans l interface
 
 var (
-	mu          sync.RWMutex
-	myIP        string
-	localClient tailscale.LocalClient
-	//loadError    = false
-	//needsLogin   = false
+	mu           sync.RWMutex
+	myIP         string
+	localClient  tailscale.LocalClient
 	errorMessage = ""
-
-	//exitNodePing   = 0.0
 )
-
-// set login-url as a variable in registry
-
-// implement the OIDC scenario to
-
-// add an entry to specify a preshared key
 
 // tailscale local client to use for IPN
 
-func main() {
-
+func exitIfAlreadyRunnning() {
 	addr := "localhost:25169"
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -63,7 +51,12 @@ func main() {
 		os.Exit(1)
 	}
 	defer l.Close()
-	// your program logic here
+}
+
+func main() {
+
+	exitIfAlreadyRunnning()
+	// load environement parameters from %programdata%\.env
 	loadEnv()
 
 	latencies = make(map[string][]float64)
@@ -76,103 +69,27 @@ func main() {
 	systray.Run(onReady, nil)
 }
 
-// change the function to pass mandatory parameters with login-url
-func doConnectionControl(m *systray.MenuItem, verb string) {
-	for {
-		if _, ok := <-m.ClickedCh; !ok {
-			break
-		}
-		bsBefore := getBackenState()
-		log.Printf("state before : %s", bsBefore)
-		//log.Printf("launch command: tailscale %s", verb)
-		_, err := execCommand(cliExecutable, verb)
+func onReady() {
+
+	log.Printf("getting localClient...")
+	getStatus := localClient.Status
+
+	st, _ := getStatus(context.TODO())
+
+	SetupMenuGL()
+	// compose complete menu with hidden options
+	AddConnectionHandlersToMenu()
+	AddExitNodeHandlersToMenu()
+	sm.SetHandler("ADMIN", func() {
+		err := openBrowser(adminUrl)
 		if err != nil {
 			Notify(err.Error())
 		}
-		bsAfter := getBackenState()
-		log.Printf("state after : %s", bsAfter)
-		if bsBefore != bsAfter {
-			if bsAfter == "Running" {
-				setExitNode()
-				Notify("Cyber Vpn is active with exit node")
-			} else {
-				// TODO: faire plutot un switch avec default
-				if strings.ToLower(bsAfter) == "needslogin" {
-					Notify(fmt.Sprintf("Cyber Vpn needs login ,\n click on systray icon to log"))
-				}
-				if strings.ToLower(bsAfter) == "stopped" {
-					Notify(fmt.Sprintf("Cyber Vpn is disconnected"))
-				}
-				if strings.ToLower(bsAfter) == "logedout" {
-					Notify(fmt.Sprintf("Cyber Vpn is loged out \nClick on Login when you want to activate"))
-				}
-			}
-
-		}
-		// TODO: loop with timeout for changing state
-	}
-}
-
-func exitSystray(m *systray.MenuItem) {
-	<-m.ClickedCh
-	systray.Quit()
-}
-
-// func parseForHttps(out []byte) string {
-// 	lines := strings.Split(string(out), "\n")
-// 	for _, l := range lines {
-// 		if strings.Contains(l, "https") {
-// 			return l
-// 		}
-// 	}
-// 	return ""
-// }
-
-func waitForLogin(m *systray.MenuItem) {
-	for {
-		<-m.ClickedCh
-		m.Disable()
-		doLogin()
-		// enable menu Login (it will be hidden by another routine)
-		m.Enable()
-	}
-}
-
-func waitForClickAndNotify(m *systray.MenuItem) {
-	for {
-		<-m.ClickedCh
-		beeep.Notify(
-			appName,
-			errorMessage,
-			"",
-		)
-	}
-}
-
-func waitForClickAndOpenBrowser(m *systray.MenuItem, url string) {
-	for {
-		_, ok := <-m.ClickedCh
-		if !ok {
-			break
-		}
-		err := openBrowser(url)
-		if err != nil {
-			Notify(err.Error())
-		}
-	}
-}
-
-func waitForClickAndCopyIpToClipboard(m *systray.MenuItem) {
-	for {
-		_, ok := <-m.ClickedCh
-		if !ok {
-			break
-		}
-		mu.RLock()
-		if myIP == "" {
-			mu.RUnlock()
-			continue
-		}
+	})
+	sm.SetHandler("SHOW_ERROR", func() {
+		Notify(errorMessage)
+	})
+	sm.SetHandler("MYIP", func() {
 		err := clipboard.WriteAll(myIP)
 		if err == nil {
 			beeep.Notify(
@@ -181,72 +98,9 @@ func waitForClickAndCopyIpToClipboard(m *systray.MenuItem) {
 				"",
 			)
 		}
-		mu.RUnlock()
-	}
-}
+	})
 
-func onReady() {
-
-	log.Printf("getting localClient...")
-	getStatus := localClient.Status
-
-	st, _ := getStatus(context.TODO())
-	//bs, _ := json.Marshal(st)
-	//fmt.Println(string(bs))
-
-	systray.SetIcon(iconOff)
-
-	// compose complete menu with hidden options
-
-	mError := systray.AddMenuItem("Show Error", "")
-	mError.Hide()
-	go waitForClickAndNotify(mError)
-
-	mLogin := systray.AddMenuItem("Login...", "")
-	mLogin.Hide()
-	go waitForLogin(mLogin)
-
-	systray.AddSeparator()
-	mConnect := systray.AddMenuItem("Connect", "")
-	mConnect.Hide()
-	go doConnectionControl(mConnect, "up")
-
-	mDisconnect := systray.AddMenuItem("Disconnect", "")
-	mDisconnect.Hide()
-	go doConnectionControl(mDisconnect, "down")
-
-	menuExitNode = systray.AddMenuItem("Exit Node Off", "")
-	go setExitNodeOff()
-
-	systray.AddSeparator()
-	mThisDevice := systray.AddMenuItem("This device:", "")
-	mThisDevice.Hide()
-	go waitForClickAndCopyIpToClipboard(mThisDevice)
-
-	systray.AddSeparator()
-	mNetworkDevices := systray.AddMenuItem("Network Devices", "")
-	mNetworkDevices.Show()
-	mMyDevices := mNetworkDevices.AddSubMenuItem("My Devices", "")
-	//mTailscaleServices := mNetworkDevices.AddSubMenuItem("Tailscale Services", "")
-
-	systray.AddSeparator()
-	mAdminConsole := systray.AddMenuItem("Admin Console...", "")
-	if adminMode != "on" {
-		mAdminConsole.Disable()
-	}
-	go waitForClickAndOpenBrowser(mAdminConsole, adminUrl)
-
-	systray.AddSeparator()
-	mExit := systray.AddMenuItem("Exit", "")
-	go exitSystray(mExit)
-
-	systray.AddSeparator()
-	mLogout := systray.AddMenuItem("Logout...", "")
-	mLogout.Hide()
-	go doConnectionControl(mLogout, "logout")
-
-	systray.AddSeparator()
-
+	sm.SetIcon("", iconOff)
 	if st != nil {
 		if st.BackendState == "NeedsLogin" {
 			Notify("Cyber Vpn needs you to login...")
@@ -260,68 +114,38 @@ func onReady() {
 	}
 
 	go func() {
-		type Item struct {
-			menu  *systray.MenuItem
-			title string
-			ip    string
-			found bool
-		}
-		items := map[string]*Item{}
-
 		for {
 			time.Sleep(3 * time.Second)
+
 			status, err := getStatus(context.TODO())
-			//log.Printf("%s", status.Self.HostName)
-			// find exit nodes in peers
 
 			if err != nil {
-				//loadError = true
 				errorMessage = err.Error()
 				log.Printf("%s", errorMessage)
-				systray.SetTooltip(appName + ": Api Error")
-				mConnect.Hide()
-				mDisconnect.Hide()
-				mError.Show()
-				mLogin.Hide()
-				mLogout.Hide()
-				mThisDevice.Hide()
-				mMyDevices.Hide()
-				systray.SetIcon(iconOff)
+				sm.SetHiddenAll([]string{"CONNECT", "LOGIN", "DISCONNECT", "EXITNODE_ON", "EXINODE_OFF", "LOGOUT"}, true)
+				sm.SetHidden("SHOW_ERROR", false)
+				sm.SetIcon("", iconOff)
 				continue
 			} else {
-				//loadError = false
 				errorMessage = ""
-				mError.Hide()
+				sm.SetHidden("SHOW_ERROR", true)
+				sm.SetLabel("STATUS", status.BackendState)
 				switch status.BackendState {
 				case "NeedsLogin":
-					mLogin.Show()
-					mLogout.Hide()
-					mThisDevice.Hide()
-					mConnect.Hide()
-					mDisconnect.Hide()
-					mMyDevices.Hide()
-					systray.SetTooltip(appName + ": Needs Login")
+					sm.SetHiddenAll([]string{"CONNECT", "DISCONNECT", "EXITNODE_ON", "EXINODE_OFF", "LOGOUT"}, true)
+					sm.SetHidden("LOGIN", false)
 					systray.SetIcon(iconOff)
 					continue
 				case "Stopped":
-					mLogin.Hide()
-					mLogout.Show()
-					mConnect.Show()
-					mDisconnect.Hide()
-					mMyDevices.Hide()
-					mThisDevice.Hide()
-					systray.SetIcon(iconOff)
+					sm.SetHiddenAll([]string{"DISCONNECT", "EXITNODE_ON", "EXINODE_OFF", "LOGIN"}, true)
+					sm.SetHiddenAll([]string{"LOGOUT", "CONNECT"}, false)
+					sm.SetIcon("", iconOff)
 					systray.SetTooltip(appName + ": Stopped")
 					continue
 				case "Running", "Starting":
-					mLogin.Hide()
-					mLogout.Show()
-					mConnect.Hide()
-					mDisconnect.Show()
-					mMyDevices.Show()
-					mThisDevice.Show()
-					systray.SetIcon(iconOn)
-					systray.SetTooltip(appName + ": " + status.BackendState)
+					sm.SetHiddenAll([]string{"CONNECT", "EXITNODE_ON", "EXINODE_OFF", "LOGIN"}, true)
+					sm.SetHiddenAll([]string{"LOGOUT", "DISCONNECT"}, false)
+					sm.SetIcon("", iconOn)
 				}
 			}
 
@@ -330,16 +154,8 @@ func onReady() {
 			if len(status.TailscaleIPs) != 0 {
 				myIP = status.TailscaleIPs[1].String()
 				log.Printf("my ip: %s", myIP)
+				sm.SetLabel("MYIP", myIP)
 			}
-			mu.Unlock()
-
-			for _, v := range items {
-				v.found = false
-			}
-
-			mThisDevice.SetTitle(fmt.Sprintf("This device: %s (%s)", status.Self.HostName, myIP))
-
-			mu.Lock()
 			refreshExitNodes()
 			checkLatency()
 			if status.ExitNodeStatus != nil {
---
+++ menu.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/darksip/tailscale-systray/sysmenu"
+)
+
+var (
+	sm     *sysmenu.SysMenu
+	hide   func(id string, v bool)
+	enable func(id string, v bool)
+	add    func(e sysmenu.Melt)
+	sethnd func(id string, e sysmenu.EvtHnd)
+	setlbl func(id string, lbl string)
+	setico func(id string, ico []byte)
+)
+
+// map id
+
+func buildMenu() {
+
+	sm = sysmenu.NewSysMenu(hide, enable, add, sethnd, setlbl, setico)
+
+	(*sm).Add(sysmenu.Melt{Id: "STATUS", Label: "unknown status", Disabled: true})
+	(*sm).Add(sysmenu.Melt{Id: "MYIP", Label: "0.0.0.0/32", Disabled: false})
+	(*sm).Add(sysmenu.Melt{Separator: true})
+	(*sm).Add(sysmenu.Melt{Id: "EXITNODES", Label: "-- Exit Nodes --", Disabled: true})
+	(*sm).Add(sysmenu.Melt{Id: "EN1", Label: "", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "EN2", Label: "", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "EN3", Label: "", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "EN4", Label: "", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "EN5", Label: "", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Separator: true})
+	(*sm).Add(sysmenu.Melt{Id: "SHOW_ERROR", Label: "Show last error...", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "ADMIN", Label: "Admin Portal", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Separator: true})
+	(*sm).Add(sysmenu.Melt{Id: "LOGIN", Label: "Login", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "CONNECT", Label: "Connect", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "DISCONNECT", Label: "Disconnect", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "EXITNODE_ON", Label: "Activate Exit Node", Hidden: true})
+	(*sm).Add(sysmenu.Melt{Id: "EXITNODE_OFF", Label: "Disable Exit Node", Hidden: true})
+
+	(*sm).Add(sysmenu.Melt{Separator: true})
+	(*sm).Add(sysmenu.Melt{Id: "EXIT", Label: "Exit"})
+	(*sm).Add(sysmenu.Melt{Id: "LOGOUT", Label: "Logout", Hidden: true})
+
+}
